9f432a5d8c388706435e2546181a7e55c580819e
Tibério dos Santos Ferreira <tiberio.ferreiracs@gmail.com>
2026-04-08 16:45:38 -0300

    refactor(photos): melhorar a resposta da criação de fotos eo tratamento de URLs do ActiveStorage

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -2,21 +2,20 @@ module Api
   module V1
     class PhotosController < ApplicationController
       before_action :set_sheet
+      before_action :set_photo, only: :destroy
 
       def create
         photo = @sheet.photos.new(photo_params)
-        photo.image.attach(params[:image]) if params[:image].present?
 
         if photo.save
-          render json: { data: { attributes: photo } }, status: :created
+          render json: { data: render_photo(photo) }, status: :created
         else
           render json: { errors: photo.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       def destroy
-        photo = @sheet.photos.find(params[:id])
-        photo.destroy
+        @photo.destroy
         head :no_content
       end
 
@@ -26,8 +25,27 @@ module Api
         @sheet = MonitoringSheet.find(params[:monitoring_sheet_id])
       end
 
+      def set_photo
+        @photo = @sheet.photos.find(params[:id])
+      end
+
       def photo_params
-        params.require(:photo).permit(:caption, :coord_e, :coord_n, :position)
+        params.require(:photo).permit(:caption, :coord_e, :coord_n, :position, :image)
+      end
+
+      def render_photo(photo)
+        {
+          id: photo.id,
+          type: "photo",
+          attributes: {
+            caption: photo.caption,
+            coord_e: photo.coord_e,
+            coord_n: photo.coord_n,
+            position: photo.position,
+            image_url: photo.image.attached? ? url_for(photo.image) : nil,
+            created_at: photo.created_at
+          }
+        }
       end
     end
   end
