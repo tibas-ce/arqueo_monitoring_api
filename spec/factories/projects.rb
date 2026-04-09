4bf1786d24005c01d9bb85b2a552861e9e592041
Tibério dos Santos Ferreira <tiberio.ferreiracs@gmail.com>
2026-04-09 18:51:33 -0300

    feat: atualiza factories com Company, Project e associação em MonitoringSheet

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :project do
-    name { "MyString" }
-    description { "MyText" }
-    ordinance_number { "MyString" }
-    municipality { "MyString" }
-    company { nil }
+    name { "Projeto de Monitoramento CE-060" }
+    description { "Monitoramento arqueológico da rodovia CE-060" }
+    ordinance_number { "001/2026" }
+    municipality { "Crato" }
+    association :company
   end
 end
