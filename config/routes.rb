6d7e50f2ce93c8e9bb8f9b75da850eedd6216a80
Tibério dos Santos Ferreira <tiberio.ferreiracs@gmail.com>
2026-04-13 10:33:52 -0300

    feat: adiciona rotas para API de companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :companies
       resources :monitoring_sheets, only: [ :create, :index, :show, :update, :destroy ]
       resources :monitoring_sheets do
         member do
