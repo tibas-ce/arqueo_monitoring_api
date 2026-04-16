86715625f91a87ddfe152953df3b2e86d043dc81
Tibério dos Santos Ferreira <tiberio.ferreiracs@gmail.com>
2026-04-16 14:34:44 -0300

    spec: refatora spec do model Project e adiciona testes de integridade

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,28 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  let(:project) { create(:project) }
-  it "é valido com atributos corretos" do
-    expect(project).to be_valid
+  describe "validações" do
+    it "é valido com atributos corretos" do
+      project = create(:project)
+      expect(project).to be_valid
+    end
+
+    it "valida campos obrigatórios" do
+      fields = [ :name, :ordinance_number, :municipality ]
+
+      fields.each do |field|
+        expect_invalid_without(:project, field)
+      end
+    end
   end
 
-  it "é inválido sem nome" do
-    project = build(:project, name: nil)
-    expect(project).not_to be_valid
-  end
+  describe "associações" do
+    it "é inválido sem Empresa" do
+      project = build(:project, company: nil)
+      expect(project).not_to be_valid
+    end
 
-  it "é inválido sem portaria" do
-    project = build(:project, ordinance_number: nil)
-    expect(project).not_to be_valid
-  end
+    it "pertence a uma empresa" do
+      project = create(:project)
+      expect(project.company).to be_a(Company)
+    end
 
-  it "é inválido sem município" do
-    project = build(:project, municipality: nil)
-    expect(project).not_to be_valid
-  end
+    it "aparece na lista de projetos da empresa" do
+      company = create(:company)
+      project = create(:project, company: company)
 
-  it "é inválido sem Empresa" do
-    project = build(:project, company: nil)
-    expect(project).not_to be_valid
+      expect(company.projects).to include(project)
+    end
+  end
+  describe "integridade" do
+    it "é inválido com empresa inexistente (validação do Rails)" do
+      project = build(:project, company_id: 99999)
+
+      expect(project).not_to be_valid
+      expect(project.errors[:company]).to include("must exist")
+    end
   end
 end
